3c3f2db98483f5273c33d337383246f93fa1cd10
Jairop98 <jairop98@yahoo.com>
2023-02-04 18:35:58 -0800

    first commit

---
+++ basketball.py
@@ -0,0 +1,74 @@
+class Player: 
+    def __init__(self, player_b):
+        self.name = player_b["name"]
+        self.age = player_b["age"]
+        self.position = player_b["position"]
+        self.team = player_b["team"]
+
+
+    def __repr__(self):
+        return "Player: {}, Age: {}, Position: {}, Team: {}".format(self.name, self.age, self.position, self.team)
+
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+
+player_jason = Player(jason)
+print(player_jason)
+
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33, "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32, "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "", 
+        "age":16, 
+        "position": "P", 
+        "team": "en"
+    }
+]
+
+new_team = []
+for new_player in players:
+    player = Player(new_player)
+    new_team.append(player)
+
+print(new_team)
